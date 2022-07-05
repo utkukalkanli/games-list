434606c8dc6a2c2d1d5bcdf5162f3031aa224963
utkukalkanli319 <kalkanliutku@icloud.com>
2022-07-05 15:45:31 +0300

    ADD:fragments for listing,search & detail initialized. Navigations between fragments setup

--- app/src/main/java/com/example/trendyol_internship/MainActivity.kt
+++ app/src/main/java/com/example/trendyol_internship/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.trendyol_internship
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navigationController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigationController = Navigation.findNavController(this,R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navigationController)
+    }
+
+    // kullanıcı uygulama içerisinde baska bi tarafa giderse cagırılıyor
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navigationController, null)
     }
 }
---
+++ app/src/main/java/com/example/trendyol_internship/ui/detail/DetailFragment.kt
@@ -0,0 +1,43 @@
+package com.example.trendyol_internship.ui.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.trendyol_internship.R
+import com.example.trendyol_internship.ui.listing.ListingFragmentDirections
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_listing.*
+
+
+class DetailFragment : Fragment() {
+    private var gameID = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        /**
+        arguments?.let {
+            gameID = DetailFragmentArgs.fromBundle(it).gameID
+            println(gameID)
+        }
+        */
+        to_listing_fragment_button.setOnClickListener{
+            val action = DetailFragmentDirections.actionDetailFragmentToListingFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
