af77dffe7b0ca3bbafeb976e3ef5cc5cc7028b5b
utkukalkanli319 <kalkanliutku@icloud.com>
2022-07-19 10:36:40 +0300

    DEL: some unnecessary imports

--- app/src/main/java/com/example/trendyol_internship/data/listing/model/ApiResponse.kt
+++ app/src/main/java/com/example/trendyol_internship/data/listing/model/ApiResponse.kt
@@ -1,6 +1,5 @@
 package com.example.trendyol_internship.data.listing.model
 
-import com.google.gson.annotations.SerializedName
 
 class ApiResponse(
     val results: List<Game>
--- app/src/main/java/com/example/trendyol_internship/data/listing/model/Game.kt
+++ app/src/main/java/com/example/trendyol_internship/data/listing/model/Game.kt
@@ -1,8 +1,5 @@
 package com.example.trendyol_internship.data.listing.model
 
-import com.google.gson.annotations.SerializedName
-import java.util.*
-import kotlin.collections.ArrayList
 
 // release date
 // genres - list
@@ -11,13 +8,10 @@ import kotlin.collections.ArrayList
 // reddit url
 // website url
 class Game(
-    //@SerializedName("id")
     val id: Int?,
 
-    //@SerializedName("name")
     val name: String?,
 
-    //@SerializedName("background_image")
     val background_image: String?,
     )
 
